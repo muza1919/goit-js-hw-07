31df17735d755f621f7e41758b498d9dbd30a942
Alona <alena2003305@gmail.com>
2022-12-23 17:14:29 +0200

    First commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+  const imageMarkup = galleryItems
+    .map(({ preview, original, description }) =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href='${original}'>
+    <img class="gallery__image"
+    src ='${preview}' 
+    alt ='${description}' 
+    data-source='${original}'
+    />
+  </a>
+</div>`
+    )
+    .join('');
+
+gallery.insertAdjacentHTML('beforeend', imageMarkup);
+gallery.addEventListener('click', onImageClick);
+
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  
+  if (!event.target.classList.contains('gallery__image')) {
+    return
+  }
+
+  const imageModal = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`);
+  imageModal.show();
+  
+
+  const links = document.querySelectorAll('.gallery__link')
+  links.forEach( link => {
+      if (link.getAttribute('href') === event.target.dataset.source) {
+          link.addEventListener('keydown', e => {
+              if (e.key === 'Escape') {
+                imageModal.close()
+              }return
+          })
+        }
+    })
+  };
