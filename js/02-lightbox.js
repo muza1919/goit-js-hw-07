2bb4d8627597b8898bfc12ea7010eb2ea52cb5ab
Alona <alena2003305@gmail.com>
2022-12-23 17:22:06 +0200

    Lightbox task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markupLightbox = galleryItems
+.map(({ preview, original, description }) => 
+`<li><a href="${original}" class="gallery__item">
+    <img src="${preview}" data-source = "${original}" alt="${description}" class="gallery__image">
+</a></li>`)
+.join('');
+gallery.insertAdjacentHTML('beforeend', markupLigthbox);
+
+new SimpleLightbox(".gallery a", {
+    captionsData: `alt`,
+    captionDelay: `250`,
+  captionPosition: `bottom`
+});
+
